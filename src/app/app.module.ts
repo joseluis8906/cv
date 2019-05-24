f03449dc78e42b1088b3fec9eae11945497036e2
Jose Luis Cáceres Esudero <joseluiscacere8906@gmail.com>
2019-05-24 16:22:39 -0500

    On branch master   Changes to be committed:  	modified:   package-lock.json  	modified:   package.json  	modified:   src/app/app-routing.module.ts  	modified:   src/app/app.component.html  	modified:   src/app/app.module.ts  	new file:   src/app/home/home.component.html  	new file:   src/app/home/home.component.scss  	new file:   src/app/home/home.component.spec.ts  	new file:   src/app/home/home.component.ts  	new file:   src/assets/IMG_20180830_073607_881.jpg  	new file:   src/assets/IMG_20180830_073719_805.jpg  	new file:   src/assets/book-pages-blurred.jpg  	new file:   src/assets/facebook-circle.png  	new file:   src/assets/laptop-devices-desk.jpg  	new file:   src/assets/linkedin-circle.png  	new file:   src/assets/maxresdefault.jpg  	new file:   src/assets/profile.jpg  	new file:   src/assets/profile.svg  	new file:   src/assets/twitter-circle.png  	new file:   src/assets/youtube-circle.png  	modified:   src/styles.scss  	modified:   src/theme.scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { PERFECT_SCROLLBAR_CONFIG, PerfectScrollbarConfigInterface, PerfectScrollbarModule } from "ngx-perfect-scrollbar";
+
+//angular material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true,
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    PerfectScrollbarModule,
   ],
-  providers: [],
+  providers: [
+    { provide: PERFECT_SCROLLBAR_CONFIG, useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
